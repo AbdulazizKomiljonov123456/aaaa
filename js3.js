a93adf70cf1f13a6f7a248fcb47ae0aef7f863a5
abdulaziz <komiljonov610@gmail.com>
2022-04-09 15:03:12 +0300

    aaaa

---
+++ js3.js
@@ -0,0 +1,32 @@
+var son = +prompt('Misolni yeching : 9 + 1')
+if (son > 10 || son < 10 && !isNaN(son)) {
+    alert('Sizning javob noto`g`ri! To`g`ri javob : 10')
+}else if (son = 10 && !isNaN(son)) {
+    alert('Sizning javob to`g`ri!')
+}else{
+    alert('Siz son kiritmadingiz')
+}
+var son1 = +prompt('Misolni yeching : 15 - 9')
+if(son1 > 6 || son1 < 6 && !isNaN(son1)){
+    alert('Sizning javob noto`g`ri! To`g`ri javob : 6')
+}else if (son1 = 6 && !isNaN(son1)) {
+    alert('Sizning javob to`g`ri!')
+}else{
+    alert('Siz son kiritmadingiz')
+}
+var son2 = +prompt('Misolni yeching : 5 * 5')
+if(son2 > 25 || son2 < 25 && !isNaN(son2)){
+    alert('Sizning javob noto`g`ri! To`g`ri javob : 25')
+}else if (son2 = 25 && !isNaN(son2)) {
+    alert('Sizning javob to`g`ri!')
+}else{
+    alert('Siz son kiritmadingiz')
+}
+var son3 = +prompt('Misolni yeching : 15 / 5')
+if (son3 > 3 || son3 < 3 && !isNaN(son3)) {
+    alert('Sizning javob noto`g`ri! To`g`ri javob : 3')
+}else if (son3 = 3 && !isNaN(son3)) {
+    alert('Sizning javob to`g`ri!')
+}else{
+    alert('Siz son kiritmadingiz')
+}
